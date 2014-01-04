9b639cb5df54bf637d5b86e9019c7f933273fd17
Michelle Hwang <michellerubyhwang@gmail.com>
2014-01-03 23:00:04 -0800

    updates to css

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 
 $(document).ready(function() {
 	$('.nav').localScroll({duration:500});
+    $('.small').css('font-size',($(window).width()*.009)+'px');
     $('.medium').css('font-size',($(window).width()*.07)+'px');
     $('.large').css('font-size',($(window).width()*.15)+'px');
 });
 
 $(window).resize(function(){
+    $('.small').css('font-size',(Math.min(100, $(window).width()*.009))+'px');
     $('.medium').css('font-size',($(window).width()*.07)+'px');
     $('.large').css('font-size',($(window).width()*.15)+'px');
-});
+});	
 

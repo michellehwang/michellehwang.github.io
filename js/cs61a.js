f69a7d44a7a9afca88301938f569b930e79f99d0
Michelle Hwang <michellerubyhwang@gmail.com>
2014-02-20 11:57:28 -0800

    adding scrolling

--- js/cs61a.js
+++ js/cs61a.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+	$('.nav').localScroll({duration:500});
+    
+
     $("button").click(function(evt){
     	var hello = evt.target;
     	$(evt.target).siblings().closest(".solution").toggleClass('hide');

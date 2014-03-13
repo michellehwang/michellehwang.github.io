fd3de88b426aa60a1dadb9819fe014db499478b1
Michelle Hwang <michellerubyhwang@gmail.com>
2014-03-12 21:04:55 -0700

    trying to make things cleaner

--- js/cs61a-test.js
+++ js/cs61a-test.js
@@ -6,4 +6,9 @@ $(document).ready(function() {
     	var hello = evt.target;
     	$(evt.target).siblings().closest(".solution").toggleClass('hide');
     });
+
+    $("#tuples.topic").load("tuples.html"); 
+    $("#control.topic").load("control.html"); 
+    $("#envs.topic").load("envs.html"); 
+    $("#lists.topic").load("lists.html"); 
 });

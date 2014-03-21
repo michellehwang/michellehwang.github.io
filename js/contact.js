abf3070fe462b1aaa850e796019ff7b79503dd30
Michelle Hwang <michellerubyhwang@gmail.com>
2014-03-21 02:25:39 -0700

    removing resume

--- js/contact.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	$("#links .resume").hover(
-		function(event) { 
-			$("#resume-span").removeClass("hide") },
-		function(event) { 
-			$("#resume-span").addClass("hide") }
-	);
-});
